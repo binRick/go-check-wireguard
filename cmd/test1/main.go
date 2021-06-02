3871438577b1a40b9a0c46b4df7aa85659647545
Your Name <you@example.com>
2021-06-02 16:24:22 -0400

    add example command

--- cmd/test1/main.go
+++ cmd/test1/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	//wg "github.com/binrick/go-check-wireguard"
-	wgt "github.com/binrick/go-check-wireguard/types"
+
+	"github.com/binrick/go-check-wireguard/types"
 	"github.com/k0kubun/pp"
 )
 
 func main() {
-	pp.Println(&wgt.EncodedKeys{})
+	pp.Println(&types.EncodedKeys{})
 
 	fmt.Println("vim-go")
 }
